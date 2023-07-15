35180b842ca3b571e807635ea5853197397b7ebf
tokotoko9981 <tokotokotmy@gmail.com>
2023-07-15 11:34:01 +0900

    200

--- ABC/ABC200/B.cpp
+++ ABC/ABC200/B.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(void){
+    long long int N, K;
+    cin >> N >> K;
+
+    for(int i = 0; i < K; i++){
+        if(N % 200 == 0) N /= 200;
+        else {
+            N *= 1000;
+            N += 200;
+        }
+    }
+
+    cout << N << endl;
+
+    return 0;
+}
