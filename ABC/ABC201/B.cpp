eabdb629fbcf797f597d3e014d7f8b0985299ed6
tokotoko9981 <tokotokotmy@gmail.com>
2023-07-15 18:28:48 +0900

    pairって便利

--- ABC/ABC201/B.cpp
+++ ABC/ABC201/B.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using v = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
 
+int main(void){
+
+    int N;
+
+    cin >> N;
+
+    vector<pair<int, string>> ST(N);
+
+    for(int i = 0; i < N; i++) {
+        cin >> ST.at(i).second >> ST.at(i).first;
+    }
+
+    sort(ST.begin(), ST.end());
+
+    cout << ST.at(N - 2).second << endl;
+
+
+    return 0;
+}
