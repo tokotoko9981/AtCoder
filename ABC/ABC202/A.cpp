313fa94b4c3fedcb6c8afa5fb920da9840417dab
tokotoko9981 <tokotokotmy@gmail.com>
2023-07-17 00:09:32 +0900

    特攻速攻わんだほーい

--- ABC/ABC202/A.cpp
+++ ABC/ABC202/A.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using v = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
 
+int main(void){
+
+    int  a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << (7 - a) + (7 - b) + (7 - c) << endl;
+
+    return 0;
+}
--- ABC/ABC202/B.cpp
+++ ABC/ABC202/B.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using v = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
+
+int main(void){
+
+    string S;
+
+    cin >> S;
+
+    reverse(S.begin(), S.end());
+
+    for(int i = 0; i < S.size(); i++) {
+        if(S.at(i) == '6') S.at(i) = '9';
+        else if(S.at(i) == '9') S.at(i) = '6';
+    }
+
+    cout << S << endl;
+    return 0;
+}
 
