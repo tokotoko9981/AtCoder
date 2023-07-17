f2400ba0aec0b6ac19c8f286fe46165e486294c1
tokotoko9981 <tokotokotmy@gmail.com>
2023-07-17 21:09:04 +0900

    HYPERif文RUSH

--- ABC/ABC203/A.cpp
+++ ABC/ABC203/A.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using v = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
 
+int main(void){
+
+    v a(3);
+
+    cin >> a.at(0) >> a.at(1) >> a.at(2);
+
+    if(a.at(0) == a.at(1)) cout << a.at(2) << endl;
+    else if(a.at(2) == a.at(1)) cout << a.at(0) << endl;
+    else if(a.at(0) == a.at(2)) cout << a.at(1) << endl;
+    else cout << 0 << endl;
+
+    return 0;
+}
--- ABC/ABC203/B.cpp
+++ ABC/ABC203/B.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using v = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
+
+int main(void){
+
+    int N, K;
+
+    cin >> N >> K;
+
+    int ans = 0;
+
+    for(int i = N; i > 0; i--){
+        for(int j = K; j > 0; j--){
+            ans += i * 100 + j;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
 
