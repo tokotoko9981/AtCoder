1d5864e3c09b172a6259f83ddafc002b6691c3e1
tokotoko9981 <tokotokotmy@gmail.com>
2023-07-18 21:48:36 +0900

    C問題で急に難易度上がるのやめろ

--- ABC/ABC204/A.cpp
+++ ABC/ABC204/A.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using v = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
 
+int main(void){
+
+    int x, y, z;
+
+    cin >> x >> y;
+
+    if(x == y) z = x;
+    else if(x + y == 3) z = 0;
+    else if(x + y == 2) z = 1;
+    else z = 2;
+
+    cout << z << endl;
+
+    return 0;
+}
--- ABC/ABC204/B.cpp
+++ ABC/ABC204/B.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using ll = long long;
+
+int main(void){
+
+    int N;
+
+    cin >> N;
+
+    vi A(N);
+
+    for(int i = 0; i < N; i++) {
+        cin >> A.at(i);
+    }
+
+    int ans = 0;
+
+    for(int i = 0; i < N; i++) {
+        if(A.at(i) > 10) ans += A.at(i) - 10;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
 
