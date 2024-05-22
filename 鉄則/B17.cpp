6be243c175581b3830df44c7376eb532cbbc0447
tokotoko9981 <tokotokotmy@gmail.com>
2024-05-22 16:58:36 +0900

    わーすーれーてーたー

---
+++ 鉄則/B17.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<long long>;
+using vll = vector<vector<long long>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using vs = vector<string>;
+using vss = vector<vector<string>>;
+using ll = long long;
+
+#define rep(o, i, n) for (ll i = o; i < (n); i++)
+#define rrep(o, i, n) for (ll i = o; i >= (n); i--)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define FI first
+#define SE second
+#define pyes cout << "Yes\n"
+#define pno cout << "No\n"
+#define YN(x) if(x) cout << "Yes\n"; else cout << "No\n"
+
+template <typename T>
+bool chmax(T &a,const T& b){if(a<b){a=b;return true;}return false;}
+template <typename T>
+bool chmin(T &a,const T& b){if(a>b){a=b;return true;}return false;}
+
+void setup(){
+#ifdef LOCAL
+    ifstream inputFile("input.txt");
+        cin.rdbuf(inputFile.rdbuf());
+#else
+    cin.tie(0); ios::sync_with_stdio(0);
+#endif
+    cout<<fixed<<setprecision(10);
+}
+
+
+
+
+int main(void){
+    setup();
+
+    ll n;
+    cin >> n;
+    vl h(n);
+    rep(0, i, n) cin >> h[i];
+    vl dp(n);
+    dp[0] = 0;
+    dp[1] = abs(h[0] - h[1]);
+    rep(2, i, n) dp[i] = min(abs(h[i - 1] - h[i]) + dp[i - 1], abs(h[i - 2] - h[i]) + dp[i - 2]);
+
+    vl ans;
+    ll now = n - 1;
+    while(true) {
+        ans.push_back(now + 1);
+        if(now == 0) break;
+        if(abs(h[now - 1] - h[now]) + dp[now - 1] == dp[now]) now--;
+        else now -= 2;
+    }
+
+    cout << ans.size() << endl;
+    rrep(ans.size() - 1, i, 0) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
