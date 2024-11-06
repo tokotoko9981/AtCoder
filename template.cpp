89c78fce05b8ce1e938d3b8d3e4b9865b9a4596a
tokotoko9981 <tokotokotmy@gmail.com>
2024-11-07 08:54:55 +0900

    テンプレートを追加

---
+++ template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using vl = vector<long long>;
+using vll = vector<vector<long long>>;
+using vc = vector<char>;
+using vcc = vector<vector<char>>;
+using vs = vector<string>;
+using vss = vector<vector<string>>;
+using ll = long long;
+const ll INF = (1LL << 60);
+
+#define rep(o, i, n) for (ll i = o; i < (n); i++)
+#define rrep(o, i, n) for (ll i = o; i >= (n); i--)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define FI first
+#define SE second
+#define pyes cout << "Yes\n"
+#define pno cout << "No\n"
+#define YN(x) if(x) cout << "Yes\n"; else cout << "No\n"
+
+template <typename T>
+bool chmax(T &a,const T& b){if(a<b){a=b;return true;}return false;}
+template <typename T>
+bool chmin(T &a,const T& b){if(a>b){a=b;return true;}return false;}
+
+__attribute__((constructor)) void constructor() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(16);
+}
+
+
+
+int main() {
+
+    return 0;
+}
